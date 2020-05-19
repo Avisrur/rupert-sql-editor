048a6a4122e206aaaf54c010a60df32ece3b6091
Avisrur <nadavavisrur@gmail.com>
2020-05-19 14:50:32 +0300

    added gitignore

---
+++ client/build/precache-manifest.6e5375722a46f53460fa89975ff6e9f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dbc8b6ec775be3501fb2bfa612880d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "389a0903986d89298b3b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81a3e7a71882285f6872",
+    "url": "/static/js/2.f233bcf4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f233bcf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389a0903986d89298b3b",
+    "url": "/static/js/main.f15275cf.chunk.js"
+  },
+  {
+    "revision": "cabd02fc3a0cafccf3da",
+    "url": "/static/js/runtime-main.a98ff6dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
