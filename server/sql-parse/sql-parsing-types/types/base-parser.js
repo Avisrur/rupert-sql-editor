08d7a6f9f3ebe60d5cce46f73431593d8da21698
Avisrur <nadavavisrur@gmail.com>
2020-05-27 12:38:57 +0300

    Update base-parser.js

---
+++ server/sql-parse/sql-parsing-types/types/base-parser.js
@@ -0,0 +1,1 @@
+//wanted to make an interface with parse() function and every parser would implement would have to implement parse() function, but didnt know and couldnt find how to make interface on nodejs
